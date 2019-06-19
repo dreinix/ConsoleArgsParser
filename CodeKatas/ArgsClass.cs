095c7ad26ba61034936fd50cd066d9a52e3548b9
dreinix <ricardo.aar@outlook.com>
2019-06-19 02:49:04 -0300

    Bool parse and base for general

--- CodeKatas/ArgsClass.cs
+++ CodeKatas/ArgsClass.cs
@@ -5,25 +5,81 @@ using System.Text;
 namespace CodeKatas
 {
     public class InvalidSchemeException : Exception { };
-    public class ArgsClass
+    public class NoSchemeException : Exception { };
+    public class InvalidArgException : Exception { };
+
+
+    public class ArgsClass<S>
     {   
         
+        public bool IsValueType<T>(T obj)
+        {
+            return typeof(S).IsValueType;
+        }
 
-        public Dictionary<string, bool> Args(string[] scheme, string args)
+
+        public Dictionary<string, S> GeneralSParse(Dictionary<string, S> scheme, string args)
+        {
+            var dict = new Dictionary<string, S>();
+
+            if (scheme.Count == 0 || scheme == null)
+                throw new NoSchemeException();
+            
+            foreach (var command in scheme)
+            {   
+                if (command.Key.Length != 2 || command.Key[0] != '-')
+                    throw new InvalidSchemeException();
+
+                else
+                {
+
+                }
+            }
+            if (args != null)
+            {
+                string[] chain = args.Split(" ");
+                foreach (var item in chain)
+                {
+
+                }
+            }
+           return dict;
+        }
+
+        public Dictionary<string, bool> BoolArgs(string[] scheme, string args)
         {
             var dict = new Dictionary<string, bool>();
 
-            if (scheme.Length == 0)
-                throw new Exception("No scheme");     
+            if (scheme.Length == 0 || scheme == null)
+                throw new NoSchemeException();
 
             foreach (string command in scheme)
             {
-                if (command.Length != 2 && command[0] != '-')
+                if (command.Length != 2 || command[0] != '-')
                     throw new InvalidSchemeException();
                 else
                 {
                     dict.Add(command, false);
+                }
+            }
+            if (args!=null)
+            {
+                string[] chain = args.Split(" ");
+                foreach (var item in chain)
+                {   
+                    if(item.Length!=2 && item[0]!='-')
+                        throw new InvalidArgException();
 
+                    if (dict.ContainsKey(item))
+                    {
+                        dict.Remove(item);
+                        dict.Add(item, true);
+                    }
+                    else
+                    {
+                        throw new InvalidArgException();
+                    }
+                    
                 }
             }
             return dict;
--- NTest/Program.cs
+++ NTest/Program.cs
@@ -5,30 +5,27 @@ using System.Collections.Generic;
 
 namespace NTest
 {
+    //Group Only Bools
     [TestFixture]
-    class Program
+    class BoolsTest
     {
         [Test]
-        public void EmtyString()
+        public void EmptyScheme()
         {
             var scheme = new string[0];
-            var args = "";
-            var control = new ArgsClass();
-            var expected = new Exception("No scheme");
-            var actual = control.Args(scheme, args);
-
-        
+            var control = new ArgsClass<bool>();
+            Assert.Throws<NoSchemeException>(() => control.BoolArgs(scheme, null));
         }
         [Test]
         public void OneScheme()
         {
             var scheme = new string[1];
             scheme[0] = "-p";
-            var control = new ArgsClass();
+            var control = new ArgsClass<bool>();
             var expected = new Dictionary<string, bool>();
             expected.Add(scheme[0], false);
 
-            var actual = control.Args(scheme, null);
+            var actual = control.BoolArgs(scheme, null);
             Assert.AreEqual(expected, actual);
         }
 
@@ -38,12 +35,12 @@ namespace NTest
             var scheme = new string[2];
             scheme[0] = "-p";
             scheme[1] = "-x";
-            var control = new ArgsClass();
+            var control = new ArgsClass<bool>();
             var expected = new Dictionary<string, bool>();
             expected.Add(scheme[0], false);
             expected.Add(scheme[1], false);
 
-            var actual = control.Args(scheme, null);
+            var actual = control.BoolArgs(scheme, null);
             Assert.AreEqual(expected, actual);
         }
 
@@ -53,13 +50,89 @@ namespace NTest
             var scheme = new string[2];
             scheme[0] = "-px";
             scheme[1] = "-x";
-            var control = new ArgsClass();
+            var control = new ArgsClass<bool>();
             var expected = new Dictionary<string, bool>();
             expected.Add(scheme[0], false);
             expected.Add(scheme[1], false);
 
-            Assert.Throws<InvalidSchemeException>(() => control.Args(scheme, null));
+            Assert.Throws<InvalidSchemeException>(() => control.BoolArgs(scheme, null));
         }
+        [Test]
+        public void NoArgs()
+        {
+            var scheme = new string[2];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            string arg = "-p";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
 
+            Assert.AreEqual(expected, control.BoolArgs(scheme, arg));
+        }
+        [Test]
+        public void SomeArgs()
+        {
+            var scheme = new string[4];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            scheme[2] = "-y";
+            scheme[3] = "-z";
+            string arg = "-p -y";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+            expected.Add(scheme[2], true);
+            expected.Add(scheme[3], false);
+            Assert.AreEqual(expected, control.BoolArgs(scheme, arg));
+        }
+        [Test]
+        public void InvalidArgs()
+        {
+            var scheme = new string[4];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            scheme[2] = "-y";
+            scheme[3] = "-z";
+            string arg = "-p x";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+            expected.Add(scheme[2], true);
+            expected.Add(scheme[3], false);
+
+            Assert.Throws<InvalidArgException>(() => control.BoolArgs(scheme, arg));
+        }
+        [Test]
+        public void InvalidArgsNotDeclared()
+        {
+            var scheme = new string[4];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            scheme[2] = "-y";
+            scheme[3] = "-z";
+            string arg = "-p -x -a";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+            expected.Add(scheme[2], true);
+            expected.Add(scheme[3], false);
+
+            Assert.Throws<InvalidArgException>(() => control.BoolArgs(scheme, arg));
+        }
+    }
+    [TestFixture]
+    class IntTests{
+        [Test]
+        public void SomeScheme()
+        {
+            var scheme = new string[0];
+            var control = new ArgsClass<bool>();
+            Assert.Throws<NoSchemeException>(() => control.BoolArgs(scheme, null));
+        }
     }
 }
