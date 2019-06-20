652d794461f872a9bd7a166205c20b69d2d60232
dreinix <ricardo.aar@outlook.com>
2019-06-19 22:11:22 -0300

    FinishedGenericCases
    Se terminaron los casos para los tipos de datos genericos

---
+++ ConsoleArgsParser/CodeKatas/ArgsClass.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeKatas
+{
+    public class InvalidSchemeException : Exception { };
+    public class NoSchemeException : Exception { };
+    public class InvalidArgException : Exception { };
+
+
+    public class ArgsClass
+    {   
+        private Dictionary<string, string> schems = new Dictionary<string, string>();
+
+        public void AddScheme<S>(Dictionary<string, S> scheme)
+        {
+            if (scheme.Count == 0 || scheme == null)
+                throw new NoSchemeException();
+
+            foreach (var command in scheme)
+            {
+                if (command.Key.Length != 2 || command.Key[0] != '-')
+                    throw new InvalidSchemeException();
+
+                else
+                {
+                    Type ParameterType = default(S).GetType();
+                    if (ParameterType == typeof(bool))
+                        schems.Add(command.Key, "false");
+                    else
+                        schems.Add(command.Key, "");
+                }
+            }
+        }
+        public Dictionary<string,string> GetSchemes()
+        {
+            return schems;
+        }
+        private string[] chain;
+
+        public Dictionary<string, string> GeneralSParse(string args)
+        {
+            var dict = new Dictionary<string, string>();
+            if (args != null || args.Length == 0)
+            {
+                chain = args.Split(" ");
+                int cant = chain.Length;
+                //if()
+                for (int i = 0; i < cant; i++)
+                {
+                    if (schems.ContainsKey(chain[i]))
+                    {
+                        bool bollean = false;
+                        if (schems[chain[i]] == "false" || schems[chain[i]] == "true")
+                            bollean = true;
+                        if (!bollean)
+                        {
+                            if (i == cant - 1)
+                                throw new InvalidArgException();
+                            if (i != cant - 1 && chain[i + 1][0] == '-')
+                                throw new InvalidArgException();
+
+                        }
+                        if (chain[i].Length != 2 && chain[i][0] != '-')
+                        {
+                            throw new InvalidArgException();
+                        }/*
+                        if ((schems[chain[i]]=="false" || schems[chain[i]] == "true"))
+                        {   //if(chain[i + 1][0] == '-')
+                                throw new InvalidArgException();
+                        }*/
+
+                        //dict.Remove(chain[i]);
+                        if (schems[chain[i]] == "false")
+                            dict.Add(chain[i], "true");
+                        else
+                        {
+                            string value = chain[i + 1].ToString();
+                            dict.Add(chain[i], value);
+                            i++;
+                        }
+
+                    }
+                    else
+                    {
+                        throw new InvalidArgException();
+                    }
+                }
+            }
+            return dict;
+        }
+
+
+        public Dictionary<string, string> GeneralSParse(string[] args)
+        {
+            var dict = new Dictionary<string, string>();           
+            if (args != null || args.Length==0)
+            {                  
+                int cant = chain.Length;
+                //if()
+                for(int i = 0;i< cant; i++)
+                {   
+                    if (schems.ContainsKey(chain[i]))
+                    {
+                        bool bollean = false;
+                        if (schems[chain[i]] == "false" || schems[chain[i]] == "true")
+                            bollean = true;
+                        if (!bollean)
+                        {
+                            if (i == cant - 1)
+                                throw new InvalidArgException();
+                            if (i != cant - 1 && chain[i + 1][0] == '-')
+                                throw new InvalidArgException();
+
+                        }
+                        if(chain[i].Length != 2 && chain[i][0] != '-')
+                        {
+                            throw new InvalidArgException();
+                        }/*
+                        if ((schems[chain[i]]=="false" || schems[chain[i]] == "true"))
+                        {   //if(chain[i + 1][0] == '-')
+                                throw new InvalidArgException();
+                        }*/
+                        dict.Remove(chain[i]);
+                        if (schems[chain[i]] == "false")
+                            dict.Add(chain[i], "true");
+                        else
+                        {
+                            string value = chain[i + 1].ToString();
+                            dict.Add(chain[i],value);
+                            i++;
+                        }                           
+                        
+                    }
+                    else
+                    {
+                        throw new InvalidArgException();
+                    }
+                }
+            }
+           return dict;
+        }
+
+        public Dictionary<string, bool> BoolArgs(string[] scheme, string args)
+        {
+            var dict = new Dictionary<string, bool>();
+
+            if (scheme.Length == 0 || scheme == null)
+                throw new NoSchemeException();
+
+            foreach (string command in scheme)
+            {
+                if (command.Length != 2 || command[0] != '-')
+                    throw new InvalidSchemeException();
+                else
+                {
+                    dict.Add(command, false);
+                }
+            }
+            if (args!=null)
+            {
+                string[] chain = args.Split(" ");
+                foreach (var item in chain)
+                {   
+                    if(item.Length!=2 && item[0]!='-')
+                        throw new InvalidArgException();
+
+                    if (dict.ContainsKey(item))
+                    {
+                        dict.Remove(item);
+                        dict.Add(item, true);
+                    }
+                    else
+                    {
+                        throw new InvalidArgException();
+                    }
+                    
+                }
+            }
+            return dict;
+        }
+    }
+}
---
+++ ConsoleArgsParser/NTest/Program.cs
@@ -0,0 +1,225 @@
+﻿using CodeKatas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace NTest
+{
+    //Group Only Bools
+    [TestFixture]
+    class BoolsTest
+    {
+        [Test]
+        public void EmptyScheme()
+        {
+            var scheme = new string[0];
+            var control = new ArgsClass();
+            Assert.Throws<NoSchemeException>(() => control.BoolArgs(scheme, null));
+        }
+        [Test]
+        public void OneScheme()
+        {
+            var scheme = new string[1];
+            scheme[0] = "-p";
+            var control = new ArgsClass();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], false);
+
+            var actual = control.BoolArgs(scheme, null);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TwoScheme()
+        {
+            var scheme = new string[2];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            var control = new ArgsClass();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], false);
+            expected.Add(scheme[1], false);
+
+            var actual = control.BoolArgs(scheme, null);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InvalidScheme()
+        {
+            var scheme = new string[2];
+            scheme[0] = "-px";
+            scheme[1] = "-x";
+            var control = new ArgsClass();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], false);
+            expected.Add(scheme[1], false);
+
+            Assert.Throws<InvalidSchemeException>(() => control.BoolArgs(scheme, null));
+        }
+        [Test]
+        public void NoArgs()
+        {
+            var scheme = new string[2];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            string arg = "-p";
+            var control = new ArgsClass();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+
+            Assert.AreEqual(expected, control.BoolArgs(scheme, arg));
+        }
+        [Test]
+        public void SomeArgs()
+        {
+            var scheme = new string[4];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            scheme[2] = "-y";
+            scheme[3] = "-z";
+            string arg = "-p -y";
+            var control = new ArgsClass();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+            expected.Add(scheme[2], true);
+            expected.Add(scheme[3], false);
+            Assert.AreEqual(expected, control.BoolArgs(scheme, arg));
+        }
+        [Test]
+        public void InvalidArgs()
+        {
+            var scheme = new string[4];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            scheme[2] = "-y";
+            scheme[3] = "-z";
+            string arg = "-p x";
+            var control = new ArgsClass();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+            expected.Add(scheme[2], true);
+            expected.Add(scheme[3], false);
+
+            Assert.Throws<InvalidArgException>(() => control.BoolArgs(scheme, arg));
+        }
+        [Test]
+        public void InvalidArgsNotDeclared()
+        {
+            var scheme = new string[4];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            scheme[2] = "-y";
+            scheme[3] = "-z";
+            string arg = "-p -x -a";
+            var control = new ArgsClass();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+            expected.Add(scheme[2], true);
+            expected.Add(scheme[3], false);
+
+            Assert.Throws<InvalidArgException>(() => control.BoolArgs(scheme, arg));
+        }
+    }
+    [TestFixture]
+    class GeneralTests{
+        [Test]
+        public void boolScheme()
+        {
+            var scheme = new Dictionary<string, bool>();
+            scheme.Add("-p", new bool());
+            var control = new ArgsClass();
+            control.AddScheme(scheme);
+            var Expected = new Dictionary<string, string>();
+            Expected.Add("-p", "false");
+            Assert.AreEqual(Expected, control.GetSchemes());
+        }
+        [Test]
+        public void intScheme()
+        {
+            var scheme = new Dictionary<string, int>();
+            scheme.Add("-e", new int());
+            var control = new ArgsClass();
+            control.AddScheme<int>(scheme);
+            var Expected = new Dictionary<string, string>();
+            Expected.Add("-e", "");
+            Assert.AreEqual(Expected, control.GetSchemes());
+        }
+        [Test]
+        public void SomeTypeScheme()
+        {
+            var scheme = new Dictionary<string, int>();
+            scheme.Add("-e", new int());
+            var control = new ArgsClass();
+            control.AddScheme<int>(scheme);
+            
+            var scheme2 = new Dictionary<string, bool>();
+            scheme2.Add("-b", new bool());
+            control.AddScheme<bool>(scheme2);
+            var Expected = new Dictionary<string, string>();
+
+            Expected.Add("-e", "");
+            Expected.Add("-b", "false");
+            Assert.AreEqual(Expected, control.GetSchemes());
+        }
+        [Test]
+        public void ArgWithMoreThanOneType()
+        {
+            var Expected = new Dictionary<string, string>();
+
+            var control = new ArgsClass();
+            control.AddScheme<int>(new Dictionary<string, int>() { { "-e", new int() } });
+            control.AddScheme<bool>(new Dictionary<string, bool>() { { "-b", new bool() } });
+            string Arg = "-e 8080 -b";
+            Expected.Add("-e", "8080");
+            Expected.Add("-b", "true");
+            Assert.AreEqual(Expected, control.GeneralSParse(Arg));
+        }
+        [Test]
+        public void ArgWithMoreThanOneTypeII()
+        {
+            var Expected = new Dictionary<string, string>();
+
+            var control = new ArgsClass();
+            control.AddScheme<int>(new Dictionary<string, int>() { { "-e", new int() }, { "-d", new int() } });
+            control.AddScheme<bool>(new Dictionary<string, bool>() { { "-b", new bool() } });
+
+
+            string Arg = "-e 8080 -b -d 1250";
+            Expected.Add("-e", "8080");
+            Expected.Add("-d", "1250");
+            Expected.Add("-b", "true");
+            Assert.AreEqual(Expected, control.GeneralSParse(Arg));
+        }
+        [Test]
+        public void ArgWithMoreThanOneTypeAndTwoLessSign()
+        {
+            var Expected = new Dictionary<string, string>();
+
+            var control = new ArgsClass();
+            control.AddScheme<int>(new Dictionary<string, int>() { { "--e", new int() }, { "--d", new int() } });
+            control.AddScheme<bool>(new Dictionary<string, bool>() { { "--b", new bool() } });
+
+
+            string Arg = "--e 8080 --b --d 1250";
+            Expected.Add("--e", "8080");
+            Expected.Add("--d", "1250");
+            Expected.Add("--b", "true");
+            Assert.AreEqual(Expected, control.GeneralSParse(Arg));
+        }
+
+        [Test]
+        public void InvalidArgWithSomeSchemes()
+        {
+            var control = new ArgsClass();
+            control.AddScheme<int>(new Dictionary<string, int>() { { "-e", new int() }, { "-d", new int() } });
+            control.AddScheme<bool>(new Dictionary<string, bool>() { { "-b", new bool() } });
+            string Arg = "-e 8080 -b -d";
+            Assert.Throws<InvalidArgException>(() => control.GeneralSParse(Arg));
+        }
+    }
+}
